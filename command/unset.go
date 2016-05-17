3166c8630de85145922a883c9304a1cc8667a3dc
Daisuke Fujita <dtanshi45@gmail.com>
2016-05-17 20:27:06 +0900

    Implement unset command

--- command/unset.go
+++ command/unset.go
@@ -1,7 +1,14 @@
 package command
 
 import (
+	"flag"
+	"fmt"
+	"os"
 	"strings"
+
+	"github.com/dtan4/k8sec/k8s"
+	"k8s.io/kubernetes/pkg/api"
+	client "k8s.io/kubernetes/pkg/client/unversioned"
 )
 
 type UnsetCommand struct {
@@ -9,7 +16,67 @@ type UnsetCommand struct {
 }
 
 func (c *UnsetCommand) Run(args []string) int {
-	// Write your code here
+	var (
+		arguments  []string
+		kubeconfig string
+		kubeClient *client.Client
+	)
+
+	flags := flag.NewFlagSet("list", flag.ContinueOnError)
+	flags.Usage = func() {}
+
+	flags.StringVar(&kubeconfig, "kubeconfig", "", "Path to the kubeconfig file")
+
+	if err := flags.Parse(args[0:]); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	for 0 < flags.NArg() {
+		arguments = append(arguments, flags.Arg(0))
+		flags.Parse(flags.Args()[1:])
+	}
+
+	if len(arguments) < 2 {
+		fmt.Fprintln(os.Stderr, "Too few arguments. Example: $ k8sec unset rails RAILS_ENV")
+		return 1
+	}
+
+	name := arguments[0]
+
+	kubeClient, err := k8s.NewKubeClient(kubeconfig)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	s, err := kubeClient.Secrets(api.NamespaceDefault).Get(name)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	for _, k := range arguments[1:] {
+		_, ok := s.Data[k]
+
+		if !ok {
+			fmt.Fprintln(os.Stderr, "The key "+k+" does not exist.")
+			return 1
+		}
+
+		delete(s.Data, k)
+	}
+
+	_, err = kubeClient.Secrets(api.NamespaceDefault).Update(s)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	fmt.Println(s.Name)
 
 	return 0
 }
