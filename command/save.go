1a48e03fbf5c541a62e8bcd15252fdac2c7501de
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-20 15:52:56 +0900

    Show variables with k=v format

--- command/save.go
+++ command/save.go
@@ -1,9 +1,15 @@
 package command
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+
+	"github.com/dtan4/k8sec/k8s"
+	"k8s.io/kubernetes/pkg/api"
+	client "k8s.io/kubernetes/pkg/client/unversioned"
 )
 
 type SaveCommand struct {
@@ -11,9 +17,61 @@ type SaveCommand struct {
 }
 
 func (c *SaveCommand) Run(args []string) int {
-	fmt.Fprintln(os.Stderr, "save command is currently not implemented.")
+	var (
+		arguments     []string
+		base64encoded bool
+		kubeconfig    string
+		kubeClient    *client.Client
+		namespace     string
+	)
 
-	return 1
+	flags := flag.NewFlagSet("list", flag.ContinueOnError)
+	flags.Usage = func() {}
+
+	flags.BoolVar(&base64encoded, "base64", false, "If true, values are parsed as base64-encoded string (Default: false)")
+	flags.StringVar(&kubeconfig, "kubeconfig", "", "Path to the kubeconfig file (Default: ~/.kube/config)")
+	flags.StringVar(&namespace, "namespace", "", "Namespace scope (Default: default)")
+
+	if err := flags.Parse(args[0:]); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	for 0 < flags.NArg() {
+		arguments = append(arguments, flags.Arg(0))
+		flags.Parse(flags.Args()[1:])
+	}
+
+	if len(arguments) > 2 {
+		fmt.Fprintln(os.Stderr, "Too many arguments.")
+		return 1
+	}
+
+	if namespace == "" {
+		namespace = api.NamespaceDefault
+	}
+
+	kubeClient, err := k8s.NewKubeClient(kubeconfig)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	secrets, err := kubeClient.Secrets(namespace).List(api.ListOptions{})
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	for _, secret := range secrets.Items {
+		for key, value := range secret.Data {
+			fmt.Println(key + "=" + strconv.Quote(string(value)))
+		}
+	}
+
+	return 0
 }
 
 func (c *SaveCommand) Synopsis() string {
