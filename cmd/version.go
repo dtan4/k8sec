765d03a4dbfff4f2eff121681a026e76a0e8de0e
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-10 20:06:43 +0900

    Add modifier

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ import (
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number",
+	Short: "Print the version number of k8sec",
 	Run:   doVersion,
 }
 
