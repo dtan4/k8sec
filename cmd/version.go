a7646cfe0ed4c1eab0cb364be2c15de2eb4f5dc7
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-10 20:06:04 +0900

    Update version command description

--- cmd/version.go
+++ cmd/version.go
@@ -10,14 +10,8 @@ import (
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: doVersion,
+	Short: "Print the version number",
+	Run:   doVersion,
 }
 
 func doVersion(cmd *cobra.Command, args []string) {
