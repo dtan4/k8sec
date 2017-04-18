604a464a9d720b8c80d0688189f5b525a41fe3f3
Daisuke Fujita <dtanshi45@gmail.com>
2017-04-18 18:21:11 +0900

    Modify application name in version command

--- version/version_test.go
+++ version/version_test.go
@@ -8,7 +8,7 @@ func TestString(t *testing.T) {
 	Version = "v0.1.0"
 	Revision = "abcd1234"
 
-	expected := "Valec version v0.1.0, build abcd1234"
+	expected := "k8sec version v0.1.0, build abcd1234"
 	actual := String()
 
 	if actual != expected {
