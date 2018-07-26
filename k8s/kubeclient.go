3b41161fee595a6d5686408769cd37afa210f786
Daisuke Fujita <dtanshi45@gmail.com>
2018-07-27 01:50:30 +0900

    Upgrade to client-go 5.0.0

--- k8s/kubeclient.go
+++ k8s/kubeclient.go
@@ -1,9 +1,9 @@
 package k8s
 
 import (
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
 )
