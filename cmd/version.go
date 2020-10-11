98a5f191127b172d342f5573720c43233b430ddb
Daisuke Fujita <dtanshi45@gmail.com>
2020-10-12 00:58:33 +0900

    Use the given stdout in version

--- cmd/version.go
+++ cmd/version.go
@@ -12,12 +12,14 @@ func newVersionCmd(out io.Writer) *cobra.Command {
 	versionCmd := &cobra.Command{
 		Use:   "version",
 		Short: "Print the version number of k8sec",
-		Run:   doVersion,
+		Run: func(cmd *cobra.Command, args []string) {
+			runVersion(args, out)
+		},
 	}
 
 	return versionCmd
 }
 
-func doVersion(cmd *cobra.Command, args []string) {
-	fmt.Println(version.String())
+func runVersion(args []string, out io.Writer) {
+	fmt.Fprintln(out, version.String())
 }
